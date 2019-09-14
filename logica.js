6259cb432c1f57048b1e19f4cb3f29372ee4350e
RobsonGap <robsonmie@hotmail.com>
2019-09-14 17:53:28 -0300

    arquivos html and Javacript

---
+++ logica.js
@@ -0,0 +1,56 @@
+/* 
+ * Este script define a função calculate() chamada pelas rotinas de tratamento de evento
+ * no código HTML acima. A função lê valores de elementos <input>, calcula
+ * as informações de pagamento de empréstimo, exibe o resultado em elementos <span>.
+ * Também salva os dados do usuário, exibe links para financeiras e desenha um gráfico.
+ */
+function calculate() {
+    // Pesquisa elementos de entrada e saída no documento
+    var mount = document.getElementById("amount");
+    var apr = document.getElementById("apr");
+    var years = document.getElementById("years");
+    var zipcode = document.getElementById("zipcoce");
+    var payment = document.getElementById("payment");
+    var total = document.getElementById("total");
+    var totalinterest = document.getElementById("totalinterest");
+
+    // Obtém a entrada do usuário através dos elementos de entrada. Presume que tudo isso é valido.
+    //Converte os juros de porcetagem para decimais e converte taxa
+    //anual para taxa mensal. Converte o período de pagamento em anos
+    //para o número de pagamentos mensais.
+    var principal = parseFloat(amount.value);
+    var interest = parseFloat(apr.value) / 100 / 12;
+    var payments = parseFloat(years.value) * 12;
+
+    // Agora calula o valor do pagamento mensal.
+    var x = Math.pow(1 + interest, payments); // Math.pow() calcula potências
+    var monthly = (principal * x * interest) / (x - 1);
+
+    //Se o resultado é um número finito, a entrada do usuário estava correta e 
+    // temos resultados significativos para exibir
+    if (isFinite(monthly)) {
+        //  Preenche os campos de saída, arredondando para 2 casas decimais 
+        payment.innerHTML = monthly.toFixed(2);
+        total.innerHTML = (monthly * payments).toFixed(2);
+        totalinterest.innerHTML = ((monthly * payments) - principal).toFixed(2);
+
+        // Salva a entrada do usuário para que possamos recuperá-la na próxima vez que
+        // ele visitar
+        save(amount.value, apr.value, years.value, zipcode.value);
+
+        // Anúncio: localiza e exibe financeiras locais, mais ignora erros de rede
+        try { // Captura quaisquer erros que ocorram dentro destas chaves
+            getLenders(amount.value, apr.value, years.value, zipcode.value);
+        } catch (e) { /* E ignora esses erros */ }
+
+        // Por fim, traça o gráfico do saldo devedor, dos juros e dos pagamentos do capital
+        chart(principal, interest, monthly, payments);
+    } else {
+        // O resultado foi Not-a-Number ou infinito, o que significa que a entrada 
+        // estava incompleta ou era inválida. Apaga qualquer saída exibida anteriormente.
+        payment.innerHTML = ""; // Apaga o conteúdo desses elementos
+        total.innerHTML = ""
+        totalinterest.innerHTML = "";
+        chart(); // Sem argumentos, apaga o gráfico
+    }
+}
